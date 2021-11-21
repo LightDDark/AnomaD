fbb0c74c6c3a8b075315b191a36b37d3fb8883a4
LightDDark <myavraham25@gmail.com>
2021-11-21 21:43:56 +0200

    Added delete

--- Code/SimpleAnomalyDetector.h
+++ Code/SimpleAnomalyDetector.h
@@ -22,14 +22,22 @@ class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
 	vector<correlatedFeatures> cf;
 	const float thershold = 0.3;
 
+	// find the maximum deviation of points from given regression line
 	virtual float maxDev(Point** points, const Line &regLine, const int size) const {
 		float maxDev = 0;
+		// goes over points
 		for (int i = 0; i < size; i++) {
+			// calculate deviation
 			float newDev = dev(*points[i], regLine);
+			// if deviation is bigger then max found then update it
 			if (maxDev < newDev) {
 				maxDev = newDev;
 			}
+			// delete unrelevant point
+			delete points[i];
 		}
+		// delete pointer
+		delete[] points;
 		return maxDev;
 	}
 public:
