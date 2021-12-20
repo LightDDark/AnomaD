0e85e995648998aa6791b22d2dc732ba03665b3e
maayan <maayansh.ms@gmail.com>
2021-12-20 13:35:56 +0200

    added hybrid detector code + updated circle

---
+++ Code/HybridAnomalyDetector.h
@@ -0,0 +1,28 @@
+//
+// Created by Maayan Shavit on 18/12/2021.
+//
+
+#ifndef ANOMALY_DETECTOR_HYBRIDANOMALYDETECTOR_H
+#define ANOMALY_DETECTOR_HYBRIDANOMALYDETECTOR_H
+
+#include "SimpleAnomalyDetector.h"
+#include "minCircle.h"
+
+class HybridAnomalyDetector:public SimpleAnomalyDetector{
+    vector<correlatedFeatures> cf;
+    const float thresholdSimple;
+    const float thresholdHybrid;
+public:
+    HybridAnomalyDetector();
+    virtual ~HybridAnomalyDetector();
+
+    void detectCorrelatedFeatures(const TimeSeries &ts, vector<correlatedindex> corrIndexes,
+                                  vector<const vector<float> *> listOfVectors, const int numOfRows);
+
+    bool checkAnomaly(correlatedFeatures &corF, Point point);
+
+    void updateCorrMEC(Point **points, string feature1, string feature2, const int &size, float pearson);
+};
+
+
+#endif //ANOMALY_DETECTOR_HYBRIDANOMALYDETECTOR_H
