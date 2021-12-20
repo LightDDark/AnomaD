35e0e3b22eb66126095c31e730820ddd9959863b
LightDDark <myavraham25@gmail.com>
2021-12-20 18:38:24 +0200

    fixed bugs

--- Code/minCircle.cpp
+++ Code/minCircle.cpp
@@ -3,20 +3,20 @@
 #include <math.h>
 
 // returns distance between two points
-float pointDistance(Point& p1, Point& p2) {
+float pointDistance(const Point& p1, const Point& p2) {
     // calculates and return distance
     return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
 // checks if points is within or on boundary of circle
-bool isWithin(Circle c, Point& p) {
+bool isWithin(const Circle c, const Point& p) {
     // is distance is <= radius then it's within/on boundary
     return pointDistance(c.center, p) <= c.radius;
 }
 
 
 // creates a circle from three points
-Circle createCircle(Point& p1, Point& p2, Point& p3) {
+Circle createCircle(const Point& p1, const Point& p2, const Point& p3) {
     // calculates remainders from p1
     double p2XRem = p2.x - p1.x;
     double p2YRem = p2.y - p1.y;
@@ -25,31 +25,38 @@ Circle createCircle(Point& p1, Point& p2, Point& p3) {
     // calculates squares
     double rem2Srq = pow(p2XRem, 2) +  pow(p2YRem, 2);
     double rem3Srq = pow(p3XRem, 2) +  pow(p3YRem, 2);
-    double remSqr = p2XRem * p3YRem - p2YRem * p3YRem;
+    double remSqr = p2XRem * p3YRem - p2YRem * p3XRem;
     // claculates center
-    float centerX = p1.x + (p2YRem * rem2Srq - p3YRem * rem3Srq) / (2 * remSqr); 
-    float centerY = p1.y + (p3XRem * rem3Srq - p2XRem * rem2Srq) / (2 * remSqr); 
+    float centerX = p1.x + (p3YRem * rem2Srq - p2YRem * rem3Srq) / (2 * remSqr); 
+    float centerY = p1.y + (p2XRem * rem3Srq - p3XRem * rem2Srq) / (2 * remSqr); 
     Point center(centerX, centerY);
     // returns Circle with radius of distance from p1 to center
     return Circle(center, pointDistance(center, p1));
 } 
 
 // creates a circle from two points
-Circle createCircle(Point& p1, Point& p2) {
+Circle createCircle(const Point& p1, const Point& p2) {
     // center will be half way between p1 and p2
-    float xCenter = (p1.x + p2.x) / 2;
-    float yCenter = (p1.y + p2.y) / 2;
+    float xCenter = (p1.x + p2.x) / 2.0;
+    float yCenter = (p1.y + p2.y) / 2.0;
     Point center(xCenter, yCenter);
     // radius will be half the distance between p1 and p2 (both on the boundary)
-    return Circle(center, pointDistance(p1, p2));
+    return Circle(center, pointDistance(p1, p2) / 2.0);
+}
+
+// checks to see if all points is within circle
+bool isValid(const Circle& c, const vector<Point*>& points) {
+    for (Point* const& p : points) {
+        if (!isWithin(c, *p)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // in case of having size<=3 points, we calculate the circle directly
 Circle trivialMinCircle(vector<Point*>& boundaryPoints) {
     size_t size = boundaryPoints.size();
-    // make sure there's at most 3 points
-    assert(size <= 3);
-
     // calculate circle according to size
     switch (size) {
     case 0:
@@ -58,7 +65,18 @@ Circle trivialMinCircle(vector<Point*>& boundaryPoints) {
         return Circle(*boundaryPoints[0], 0);
     case 2:
         return createCircle(*boundaryPoints[0], *boundaryPoints[1]);
-    case 3: 
+    default:
+        // cheks to see if can create valid circle with only two points
+        for (size_t i = 0; i < 3; i++) {
+            for (size_t j = i + 1; j < 3; j++) {
+                Circle c = createCircle(*boundaryPoints[i], *boundaryPoints[j]);
+                if (isValid(c, boundaryPoints)) {
+                    return c;
+                }
+            }
+            
+        }
+        
         return createCircle(*boundaryPoints[0], *boundaryPoints[1],  *boundaryPoints[2]);
     }
 }
@@ -69,7 +87,7 @@ Circle trivialMinCircle(vector<Point*>& boundaryPoints) {
 * and boundary points represens the points that are sitting on
 * the boundary of the circle.
 */
-Circle welzlMiniCircle(Point** points, vector<Point*>& boundaryPoints, size_t size) {
+Circle welzlMiniCircle(Point**& points, vector<Point*> boundaryPoints, size_t size) {
     // Base - if no points left or we reached 3 points on boundary, we can calculate trivially
     if (size == 0 || boundaryPoints.size() == 3) {
         return trivialMinCircle(boundaryPoints);
